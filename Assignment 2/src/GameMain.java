48165243ca20383dfeb14a1823f095f4d7310697
MichaelHowell91 <michaelhowell@myop.ac.nz>
2022-01-06 21:25:05 +1300

    Added more '!'s to the X has won text.

--- Assignment 2/src/GameMain.java
+++ Assignment 2/src/GameMain.java
@@ -93,7 +93,7 @@ public class GameMain extends JPanel implements MouseListener{
 				statusBar.setText("It's a Draw! Click to play again.");       
 			} else if (currentState == GameState.Cross_won) {          
 				statusBar.setForeground(Color.RED);          
-				statusBar.setText("'X' Won! Click to play again.");       
+				statusBar.setText("'X' Won!!!!! Click to play again.");       
 			} else if (currentState == GameState.Nought_won) {          
 				statusBar.setForeground(Color.RED);          
 				statusBar.setText("'O' Won! Click to play again.");       
